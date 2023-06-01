4ce24535a615e762077a91603b2d0803fc443465
grunt-lucas <grunt.lucas@yahoo.com>
2023-06-01 15:13:50 -0700

    Basic getopt option parsing

--- tscreate.cpp
+++ tscreate.cpp
@@ -1,33 +1,80 @@
 #include "tscreate.h"
 
 #include <iostream>
+#include <getopt.h>
 
-#include <cxxopts.hpp>
+namespace tscreate {
+const char* const PROGRAM_NAME = "tscreate";
+const char* const VERSION = "0.0.1";
+const char* const RELEASE_DATE = "1 June 2023";
 
-int main(int argc, const char* argv[]) {
-    try {
-        std::unique_ptr<cxxopts::Options> allocated(
-            new cxxopts::Options(argv[0], "Create a pokeemerald indexed tileset from a master PNG")
-        );
-        auto& options = *allocated;
+bool gVerboseOutput = false;
 
-        options.add_options()
-            ("master", "The path to the master PNG file", cxxopts::value<std::string>())
-            ("output", "The desired output path", cxxopts::value<std::string>())
-            ("h,help", "Print usage");
-        options.parse_positional({"master", "output"});
-        options.positional_help("</path/to/master.png> </path/to/output/dir>");
+void printHelp() {
+    using std::cout;
+    using std::endl;
+    cout << PROGRAM_NAME << ", version " << VERSION << " of " << RELEASE_DATE << endl;
+    cout << "   by grunt-lucas: https://github.com/grunt-lucas/tscreate" << endl;
+    cout << endl;
+    cout << "Convert a master PNG tilesheet and optional structure file to a pokeemerald-ready indexed" << endl;
+    cout << "tileset PNG with matching palette files." << endl;
+    cout << endl;
+    cout << "Usage:  " << PROGRAM_NAME;
+    cout << " [-hstvV] ";
+    cout << "<master.png> <output-dir>" << endl;
+    cout << endl;
+    cout << "Options:" << endl;
+    cout << "   -s, --structure-file=<file>      Specify a structure PNG file." << endl;
+    cout << "   -t, --transparent-color=<R,G,B>  Specify the global transparent color. Defaults to 0,0,0." << endl;
+    cout << endl;
+    cout << "Help and Logging:" << endl;
+    cout << "   -h, --help     Print help message." << endl;
+    cout << "   -v, --version  Print version info." << endl;
+    cout << "   -V, --verbose  Enable verbose logging to stderr." << endl;
+}
 
-        auto result = options.parse(argc, argv);
+void printVersion() {
+    using std::cout;
+    using std::endl;
+    cout << PROGRAM_NAME << " " << VERSION << endl;
+}
+}
 
-        if (result.count("help")) {
-            std::cout << options.help() << std::endl;
+int main(int argc, char** argv) {
+    const char* const shortOptions = "hvV";
+    static struct option longOptions[] =
+    {
+        {"help", no_argument, nullptr, 'h'},
+        {"version", no_argument, nullptr, 'v'},
+        {"verbose", no_argument, nullptr, 'V'},
+        {nullptr, no_argument, nullptr, 0}
+    };
+
+    while (true) {
+        const auto opt = getopt_long(argc, argv, shortOptions, longOptions, nullptr);
+
+        if (opt == -1)
+            break;
+
+        switch (opt) {
+        case 'v':
+            tscreate::printVersion();
+            exit(0);
+            break;
+        case 'V':
+            tscreate::gVerboseOutput = true;
+            break;
+
+        // Help message upon '-h/--help'
+        case 'h':
+            tscreate::printHelp();
+            exit(0);
+        // Help message on invalid or unknown options
+        case '?':
+        default:
+            tscreate::printHelp();
             exit(2);
         }
-    }
-    catch (const cxxopts::exceptions::exception& e) {
-        std::cout << argv[0] << ": optarg error: " << e.what() << std::endl;
-        exit(1);
     }
 
     return 0;
