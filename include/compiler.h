f0a92caeab1efa24d8c83f829bda5b3e2d037f24
grunt-lucas <grunt.lucas@yahoo.com>
2023-08-01 06:44:31 -0700

    Remove compiler_helpers

--- include/compiler.h
+++ include/compiler.h
@@ -10,7 +10,19 @@
  * TODO : fill in doc comment
  */
 
+/*
+ * Some of the types we need are extremely verbose and confusing, so here let's define some better names to make the
+ * code a bit more readable.
+ */
+// ColorSets won't account for transparency color, we will handle that at the end
+using ColorSet = std::bitset<porytiles::MAX_BG_PALETTES *(porytiles::PAL_SIZE - 1)>;
+using DecompiledIndex = std::size_t;
+using IndexedNormTile = std::pair<DecompiledIndex, porytiles::NormalizedTile>;
+using IndexedNormTileWithColorSet = std::tuple<DecompiledIndex, porytiles::NormalizedTile, ColorSet>;
+
 namespace porytiles {
+
+extern std::size_t gRecurseCount;
 
 /**
  * TODO : fill in doc comments
