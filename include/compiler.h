780ec727305c6d2a3b576aaeec65138483c55b29
grunt-lucas <grunt.lucas@yahoo.com>
2023-08-02 16:36:17 -0700

    Added DecompIndex struct

--- include/compiler.h
+++ include/compiler.h
@@ -12,17 +12,14 @@
  * TODO : fill in doc comment
  */
 
-/*
- * Some of the types we need are extremely verbose and confusing, so here let's define some better names to make the
- * code a bit more readable.
- */
-// ColorSets won't account for transparency color, we will handle that at the end
-using ColorSet = std::bitset<porytiles::MAX_BG_PALETTES *(porytiles::PAL_SIZE - 1)>;
-using DecompiledIndex = std::size_t;
-using IndexedNormTile = std::pair<DecompiledIndex, porytiles::NormalizedTile>;
-using IndexedNormTileWithColorSet = std::tuple<DecompiledIndex, porytiles::NormalizedTile, ColorSet>;
-
 namespace porytiles {
+
+struct DecompiledIndex2 {
+  bool animated;
+  std::size_t animIndex;
+  std::size_t frameIndex;
+  std::size_t tileIndex;
+};
 
 extern std::size_t gRecurseCount;
 
@@ -32,5 +29,15 @@ extern std::size_t gRecurseCount;
 std::unique_ptr<CompiledTileset> compile(PtContext &ctx, const DecompiledTileset &decompiledTileset);
 
 } // namespace porytiles
+
+/*
+ * Some of the types we need are extremely verbose and confusing, so here let's define some better names to make the
+ * code a bit more readable.
+ */
+// ColorSets won't account for transparency color, we will handle that at the end
+using ColorSet = std::bitset<porytiles::MAX_BG_PALETTES *(porytiles::PAL_SIZE - 1)>;
+using DecompiledIndex = std::size_t;
+using IndexedNormTile = std::pair<DecompiledIndex, porytiles::NormalizedTile>;
+using IndexedNormTileWithColorSet = std::tuple<DecompiledIndex, porytiles::NormalizedTile, ColorSet>;
 
 #endif // PORYTILES_COMPILER_H
