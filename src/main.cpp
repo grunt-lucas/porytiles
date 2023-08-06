d9cad32ee5e1b715345ed073dfa1f0a348b24387
grunt-lucas <grunt.lucas@yahoo.com>
2023-08-05 21:44:47 -0700

    Updated some notes

--- src/main.cpp
+++ src/main.cpp
@@ -19,12 +19,12 @@ try {
 }
 catch (const porytiles::PtException &e) {
   // Catch PtException here, these are errors that can reasonably be expected due to bad input, bad files, etc
+  // Eventually refactor so we don't print exception message here, we will use that instead for testing
   porytiles::pt_println(stderr, "{}", e.what());
   return 1;
 }
 catch (const std::exception &e) {
-  // TODO : if this catches, something we really didn't expect happened, can we print a stack trace here? How?
-  // New C++23 features may allow this at some point: https://github.com/TylerGlaiel/Crashlogs
+  // New C++23 features may allow a stacktrace here: https://github.com/TylerGlaiel/Crashlogs
   // Or do something like this: https://stackoverflow.com/questions/691719/c-display-stack-trace-on-exception
   porytiles::pt_println(stderr, "{}: {} {}", porytiles::PROGRAM_NAME,
                         fmt::styled("internal compiler error:", fmt::emphasis::bold | fg(fmt::terminal_color::yellow)),
