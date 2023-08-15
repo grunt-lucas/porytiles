190602c4683898e8b783c5a29473ee3a6c41c71e
grunt-lucas <grunt.lucas@yahoo.com>
2023-08-15 07:33:04 -0700

    Start working on CSV parsing

--- include/utilities.h
+++ include/utilities.h
@@ -3,9 +3,19 @@
 
 #include <filesystem>
 #include <string>
+#include <unordered_map>
+
+#include "errors_warnings.h"
+#include "types.h"
 
 namespace porytiles {
 
+std::unordered_map<std::size_t, Attributes> getEmeraldRubyAttributesFromCsv(const ErrorsAndWarnings &err,
+                                                                            const InputPaths &inputs, CompilerMode mode,
+                                                                            std::string filePath);
+std::unordered_map<std::size_t, Attributes> getFireredAttributesFromCsv(const ErrorsAndWarnings &err,
+                                                                        const InputPaths &inputs, CompilerMode mode,
+                                                                        std::string filePath);
 std::filesystem::path getTmpfilePath(const std::filesystem::path &parentDir, const std::string &fileName);
 std::filesystem::path createTmpdir();
 
--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,13 +1,43 @@
 #include "utilities.h"
 
+#define FMT_HEADER_ONLY
+#include <fmt/format.h>
+
+#include <csv.h>
+#include <doctest.h>
 #include <filesystem>
 #include <random>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 
 #include "errors_warnings.h"
+#include "ptcontext.h"
+#include "types.h"
 
 namespace porytiles {
+
+std::unordered_map<std::size_t, Attributes> getEmeraldRubyAttributesFromCsv(const ErrorsAndWarnings &err,
+                                                                            const InputPaths &inputs, CompilerMode mode,
+                                                                            std::string filePath)
+{
+  std::unordered_map<std::size_t, Attributes> attributeMap{};
+  io::CSVReader<2> in{filePath};
+  try {
+    in.read_header(io::ignore_no_column, "id", "behaviour");
+  }
+  catch (const std::exception &e) {
+    fatalerror(err, inputs, mode, fmt::format("{} had incorrect header row format, expected 'id,behaviour'", filePath));
+  }
+  return attributeMap;
+}
+
+std::unordered_map<std::size_t, Attributes> getFireredAttributesFromCsv(const ErrorsAndWarnings &err,
+                                                                        const InputPaths &inputs, CompilerMode mode,
+                                                                        std::string filePath)
+{
+  throw std::runtime_error{"TODO : implement"};
+}
 
 std::filesystem::path getTmpfilePath(const std::filesystem::path &parentDir, const std::string &fileName)
 {
@@ -39,3 +69,19 @@ std::filesystem::path createTmpdir()
 }
 
 } // namespace porytiles
+
+TEST_CASE("getEmeraldRubyAttributesFromCsv should parse input CSVs as expected")
+{
+  porytiles::PtContext ctx{};
+  ctx.compilerConfig.mode = porytiles::CompilerMode::PRIMARY;
+  ctx.err.printErrors = false;
+
+  SUBCASE("It should fail on an Emerald-style CSV with no header")
+  {
+    CHECK_THROWS_WITH_AS(
+        porytiles::getEmeraldRubyAttributesFromCsv(ctx.err, ctx.inputPaths, ctx.compilerConfig.mode,
+                                                   "res/tests/csv/emerald_incorrect_1.csv"),
+        "res/tests/csv/emerald_incorrect_1.csv had incorrect header row format, expected 'id,behaviour'",
+        porytiles::PtException);
+  }
+}
