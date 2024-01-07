15f15be070b6dc254bd19439534931d100637591
grunt-lucas <grunt.lucas@yahoo.com>
2024-01-07 12:24:45 -0700

    Bump version for release

--- src/program_name.cpp
+++ src/program_name.cpp
@@ -5,7 +5,7 @@
 namespace porytiles {
 
 const std::string PROGRAM_NAME = "porytiles";
-const std::string VERSION_TAG = "1.0.0-SNAPSHOT";
-const std::string RELEASE_DATE = "---";
+const std::string VERSION_TAG = "0.0.7";
+const std::string RELEASE_DATE = "7 Jan 2024";
 
 } // namespace porytiles
