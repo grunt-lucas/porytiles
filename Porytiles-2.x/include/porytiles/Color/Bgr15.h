93985bdb3eb4cad93f50a746cc7174c7c3e3470d
grunt-lucas <grunt.lucas@yahoo.com>
2024-10-18 11:19:04 -0700

    Some initial skeleton code for 2.x, CMake config not working yet

---
+++ Porytiles-2.x/include/porytiles/Color/Bgr15.h
@@ -0,0 +1,16 @@
+#ifndef PORYTILES_COLOR_BGR15_H
+#define PORYTILES_COLOR_BGR15_H
+
+#include <cstdint>
+
+namespace porytiles {
+namespace color {
+
+class Bgr15 {
+    std::uint16_t bgr;
+};
+
+} // end namespace color
+} // end namespace porytiles
+
+#endif // PORYTILES_COLOR_BGR15_H
