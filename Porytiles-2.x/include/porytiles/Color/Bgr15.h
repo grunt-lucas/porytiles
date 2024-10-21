226a8b8705feeb1916c7b7823ca09cefc8327ec0
grunt-lucas <grunt.lucas@yahoo.com>
2024-10-21 09:28:49 -0700

    More docstrings

--- Porytiles-2.x/include/porytiles/Color/Bgr15.h
+++ Porytiles-2.x/include/porytiles/Color/Bgr15.h
@@ -6,12 +6,20 @@
 namespace porytiles::color {
 
 /**
- * @brief Value object class representing a color in 15-bit BGR format.
+ * @brief Value object representing a color in 15-bit BGR format.
  */
 class Bgr15 {
     std::uint16_t bgr;
 
   public:
+    /**
+     * @brief Default constructor that initializes the Bgr15 object to zero.
+     *
+     * This constructor sets the 15-bit BGR value to zero, resulting in a color
+     * representation with no intensity in blue, green, or red channels, i.e. black.
+     *
+     * @return A Bgr15 object with all color components set to zero.
+     */
     Bgr15() : bgr{0} {}
 
     /**
@@ -30,6 +38,14 @@ class Bgr15 {
      */
     explicit Bgr15(std::uint8_t red, std::uint8_t green, std::uint8_t blue);
 
+    /**
+     * @brief Returns the raw 16-bit BGR value.
+     *
+     * This method provides direct access to the internal 15-bit BGR representation
+     * of the color.
+     *
+     * @return The 16-bit BGR value.
+     */
     [[nodiscard]] std::uint16_t getRawValue() const;
 };
 
--- Porytiles-2.x/include/porytiles/Color/Rgba32.h
+++ Porytiles-2.x/include/porytiles/Color/Rgba32.h
@@ -7,7 +7,7 @@
 namespace porytiles::color {
 
 /**
- * @brief Value object class representing a color in 32-bit RGBA format.
+ * @brief Value object representing a color in 32-bit RGBA format.
  */
 class Rgba32 {
     std::uint8_t red;
@@ -15,18 +15,24 @@ class Rgba32 {
     std::uint8_t blue;
     std::uint8_t alpha;
 
-public:
-    Rgba32() : red{0}, green{0}, blue {0}, alpha{0} {}
+  public:
+    /**
+     * @brief Default constructor that initializes the RGBA color to all zeroes.
+     *
+     * This constructor sets the red, green, blue, and alpha components of the Rgba32 object
+     * to zero, resulting in a fully transparent black color.
+     */
+    Rgba32() : red{0}, green{0}, blue{0}, alpha{0} {}
 
-  /**
-   * @brief Converts the RGBA color to a JASC-PAL formatted string.
-   *
-   * This method converts the red, green, and blue components of the Rgba32 object
-   * into a space-separated string format commonly used in JASC-PAL color palette files.
-   *
-   * @return A string representation of the color in JASC-PAL format.
-   */
-  [[nodiscard]] std::string toJascString() const;
+    /**
+     * @brief Converts the RGBA color to a JASC-PAL formatted string.
+     *
+     * This method converts the red, green, and blue components of the Rgba32 object
+     * into a space-separated string format commonly used in JASC-PAL color palette files.
+     *
+     * @return A string representation of the color in JASC-PAL format.
+     */
+    [[nodiscard]] std::string toJascString() const;
 };
 
 } // end namespace porytiles::color
