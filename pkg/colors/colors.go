e14be5814cb96f4ea0be8eb187d233df94a280f0
grunt-lucas <grunt.lucas@yahoo.com>
2025-01-13 16:16:02 -0700

    Doc comment format updates

--- pkg/colors/colors.go
+++ pkg/colors/colors.go
@@ -5,9 +5,8 @@ Copyright © 2025 grunt-lucas grunt.lucas@yahoo.com
 // Package colors defines the various color types that Porytiles understands.
 package colors
 
-// The Rgba32 type represents a color in RGBA32 format. The type is 4 bytes
-// long: 1 byte for each color channel (red, green, blue) and 1 byte for the
-// alpha channel.
+// The Rgba32 type represents a color in RGBA32 format. The type is 4 bytes long: 1 byte for each
+// color channel (red, green, blue) and 1 byte for the alpha channel.
 type Rgba32 struct {
 	Red   uint8
 	Green uint8
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,35 +2,31 @@
 Copyright © 2025 grunt-lucas grunt.lucas@yahoo.com
 */
 
-// Package config defines the various configuration types needed to configure a
-// Porytiles job.
+// Package config defines the various configuration types needed to configure a Porytiles job.
 package config
 
-// A TilesOutputPalette represents a possible output format for Porymap-format
-// tiles (i.e. the canonical 'tiles.png' file). Vanilla tileset tiles use
-// Greyscale, but Porymap now supports tiles in TrueColor format.
+// A TilesOutputPalette represents a possible output format for Porymap-format tiles (i.e. the
+// canonical 'tiles.png' file). Vanilla tileset tiles use Greyscale, but Porymap now supports tiles
+// in TrueColor format.
 type TilesOutputPalette int
 
 const (
-	// A TrueColor 'tiles.png' contains the full palette configuration in the
-	// internal PNG palette. The bottom four bits of each pixel will correctly
-	// index into an individual palette, so 'gbagfx' will be perfectly happy.
-	// However, the top 4 bits will index into one of the internal PNG palettes.
-	// This means that the 'tiles.png' will be realistically colored, so it will
-	// be easy to see what a tile will actually look like in-game.
+	// A TrueColor 'tiles.png' contains the full palette configuration in the internal PNG palette.
+	// The bottom four bits of each pixel will correctly index into an individual palette, so
+	// 'gbagfx' will be perfectly happy. However, the top 4 bits will index into one of the internal
+	// PNG palettes. This means that the 'tiles.png' will be realistically colored, so it will be
+	// easy to see what a tile will actually look like in-game.
 	TrueColor TilesOutputPalette = iota
 
-	// A Greyscale 'tiles.png' contains a single 16-color greyscale palette.
-	// This makes it easy to see the index value of each pixel. But the tradeoff
-	// is that it's not obvious which actual palette (i.e. which color) each
-	// pixel is actually using.
+	// A Greyscale 'tiles.png' contains a single 16-color greyscale palette. This makes it easy to
+	// see the index value of each pixel. But the tradeoff is that it's not obvious which actual
+	// palette (i.e. which color) each pixel is actually using.
 	Greyscale
 )
 
-// A TargetBaseGame represents which of the three Generation III decompilation
-// projects this Porytiles job is targeting. Setting the TargetBaseGame is a
-// more convenient way to set the fieldmap parameters for a Porytiles job. Think
-// of it like a preset for the FieldmapConfig.
+// A TargetBaseGame represents which of the three Generation III decompilation projects this
+// Porytiles job is targeting. Setting the TargetBaseGame is a more convenient way to set the
+// fieldmap parameters for a Porytiles job. Think of it like a preset for the FieldmapConfig.
 type TargetBaseGame int
 
 const (
@@ -39,7 +35,7 @@ const (
 	Ruby
 )
 
-// The FieldmapConfig fields represent the fieldmap defines at the very top of
+// FieldmapConfig is Porytiles's internal representation of the fieldmap defines at the very top of
 // 'include/fieldmap.h'.
 type FieldmapConfig struct {
 	NumTilesInPrimary     int
@@ -51,8 +47,8 @@ type FieldmapConfig struct {
 	NumTilesPerMetatile   int
 }
 
-// EmeraldDefaults creates a FieldmapConfig with corresponding to Emerald's
-// 'include/fieldmap.h' configuration.
+// EmeraldDefaults creates a FieldmapConfig with corresponding to Emerald's 'include/fieldmap.h'
+// configuration.
 func EmeraldDefaults() FieldmapConfig {
 	fieldmapConfig := FieldmapConfig{}
 	fieldmapConfig.NumTilesInPrimary = 512
@@ -65,8 +61,8 @@ func EmeraldDefaults() FieldmapConfig {
 	return fieldmapConfig
 }
 
-// FireRedDefaults creates a FieldmapConfig with corresponding to FireRed's
-// 'include/fieldmap.h' configuration.
+// FireRedDefaults creates a FieldmapConfig with corresponding to FireRed's 'include/fieldmap.h'
+// configuration.
 func FireRedDefaults() FieldmapConfig {
 	fieldmapConfig := FieldmapConfig{}
 	fieldmapConfig.NumTilesInPrimary = 640
@@ -79,8 +75,8 @@ func FireRedDefaults() FieldmapConfig {
 	return fieldmapConfig
 }
 
-// RubyDefaults creates a FieldmapConfig with corresponding to Ruby's
-// 'include/fieldmap.h' configuration.
+// RubyDefaults creates a FieldmapConfig with corresponding to Ruby's 'include/fieldmap.h'
+// configuration.
 func RubyDefaults() FieldmapConfig {
 	fieldmapConfig := FieldmapConfig{}
 	fieldmapConfig.NumTilesInPrimary = 512
@@ -93,28 +89,27 @@ func RubyDefaults() FieldmapConfig {
 	return fieldmapConfig
 }
 
-// The GlobalConfig stores configuration data that can apply to a Porytiles job
-// of any type. Job-type-specific configuration data is defined separately in
-// the corresponding config structs.
+// The GlobalConfig stores configuration data that can apply to a Porytiles job of any type.
+// Job-type-specific configuration data is defined separately in the corresponding config structs.
 type GlobalConfig struct {
 	TargetBaseGame TargetBaseGame
 	FieldmapConfig FieldmapConfig
 }
 
-// NumPalettesInSecondary returns the number of secondary tileset palettes that
-// are implied by the given [FieldmapConfig] config.
+// NumPalettesInSecondary returns the number of secondary tileset palettes that are implied by the
+// given [FieldmapConfig] config.
 func (config *FieldmapConfig) NumPalettesInSecondary() int {
 	return config.NumPalettesTotal - config.NumPalettesInPrimary
 }
 
-// NumTilesInSecondary returns the number of secondary tileset tiles that are
-// implied by the given [FieldmapConfig] config.
+// NumTilesInSecondary returns the number of secondary tileset tiles that are implied by the given
+// [FieldmapConfig] config.
 func (config *FieldmapConfig) NumTilesInSecondary() int {
 	return config.NumTilesTotal - config.NumTilesInPrimary
 }
 
-// NumMetatilesInSecondary returns the number of secondary tileset metatiles
-// that are implied by the given [FieldmapConfig] config.
+// NumMetatilesInSecondary returns the number of secondary tileset metatiles that are implied by the
+// given [FieldmapConfig] config.
 func (config *FieldmapConfig) NumMetatilesInSecondary() int {
 	return config.NumMetatilesTotal - config.NumMetatilesInPrimary
 }
