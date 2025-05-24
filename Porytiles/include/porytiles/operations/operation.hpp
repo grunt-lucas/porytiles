81d1d6043b269f98ee9de10b0c2251faba6045f1
grunt-lucas <grunt.lucas@yahoo.com>
2025-05-24 11:42:25 -0700

    Added Operation class that uses gsl

--- Porytiles/include/porytiles/operations/operation.hpp
+++ Porytiles/include/porytiles/operations/operation.hpp
@@ -1,13 +1,20 @@
 #pragma once
 
+#include <gsl/pointers>
+
+#include "../diagnostics/diagnostic_engine.hpp"
 #include "../templates/any_map.hpp"
 #include "../templates/result.hpp"
 
 namespace porytiles {
 
 class Operation {
+    gsl::not_null<DiagEngine *> diag;
+
   public:
     virtual ~Operation() = default;
+
+    explicit Operation(const gsl::not_null<DiagEngine *> diag) : diag{diag} {}
 
     virtual Result<AnyMap, BinaryStatus> execute(const AnyMap &inputs) const = 0;
 };
