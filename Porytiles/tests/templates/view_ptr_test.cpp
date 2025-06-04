dac0165991d7aabcfc90b4a4fb514dfacd629ed5
grunt-lucas <grunt.lucas@yahoo.com>
2025-06-04 16:58:26 -0700

    Resharper updates

--- Porytiles/tests/templates/view_ptr_test.cpp
+++ Porytiles/tests/templates/view_ptr_test.cpp
@@ -7,9 +7,9 @@
 using namespace porytiles;
 
 TEST(ViewPtrTests, ViewPtrReadOperations) {
-    std::unique_ptr owner{std::make_unique<std::string>("foobar")};
+    const auto owner{std::make_unique<std::string>("foobar")};
 
-    view_ptr viewer{owner.get()};
+    const view_ptr viewer{owner.get()};
 
     ASSERT_EQ("foobar", *viewer);
     ASSERT_EQ("foobar", *viewer.get());
@@ -17,12 +17,12 @@ TEST(ViewPtrTests, ViewPtrReadOperations) {
 }
 
 TEST(ViewPtrTests, ViewPtrWriteOperations) {
-    std::unique_ptr owner{std::make_unique<std::string>("foobar")};
+    const auto owner{std::make_unique<std::string>("foobar")};
 
-    view_ptr viewer{owner.get()};
+    const view_ptr viewer{owner.get()};
 
     // Mutate the string through the view
-    viewer.get()->at(0) = 'g';
+    viewer->at(0) = 'g';
 
     // String should look different from view
     ASSERT_EQ("goobar", *viewer);
@@ -35,7 +35,7 @@ TEST(ViewPtrTests, ViewPtrWriteOperations) {
     ASSERT_EQ(6, owner->size());
 
     // Mutate the string through the owner
-    viewer.get()->at(5) = 't';
+    viewer->at(5) = 't';
 
     // String should look different from view
     ASSERT_EQ("goobat", *viewer);
