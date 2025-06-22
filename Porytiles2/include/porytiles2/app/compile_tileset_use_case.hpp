a82dac83aaf4464c5c871fb77a199b3dadc232b5
grunt-lucas <grunt.lucas@yahoo.com>
2025-06-22 06:28:29 -0700

    Add impl file for example app interface

--- Porytiles2/include/porytiles2/app/compile_tileset_use_case.hpp
+++ Porytiles2/include/porytiles2/app/compile_tileset_use_case.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <string>
+
 namespace porytiles {
 
-class CompileTilesetUseCase {};
+class CompileTilesetUseCase {
+  public:
+    void Compile(const std::string &tileset_name);
+};
 
 } // namespace porytiles
---
+++ Porytiles2/lib/app/compile_tileset_use_case.cpp
@@ -0,0 +1,11 @@
+#include <porytiles2/templates/panic.hpp>
+
+#include <porytiles2/app/compile_tileset_use_case.hpp>
+
+namespace porytiles {
+
+void CompileTilesetUseCase::Compile(const std::string &tileset_name) {
+    Panic("Unimplemented");
+}
+
+} // namespace porytiles
