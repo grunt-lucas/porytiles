42ba7f3bbea330bbe90041c326720e2be457ccdf
grunt-lucas <grunt.lucas@yahoo.com>
2025-06-25 12:35:31 -0700

    Compiler service pass through error strings

--- Porytiles2/lib/app/CompilePrimaryTileset.cpp
+++ Porytiles2/lib/app/CompilePrimaryTileset.cpp
@@ -14,20 +14,20 @@ Result<void> CompilePrimaryTileset::Compile(const std::string &tileset) const {
     // 1. Load the PorytilesTileset source aggregate
     auto maybe_porytiles_tileset = porytiles_repo_->Load(tileset);
     if (!maybe_porytiles_tileset.has_value()) {
-        return std::unexpected{fmt::format("failed to load tileset '{}'", tileset)};
+        return std::unexpected{maybe_porytiles_tileset.error()};
     }
     const auto porytiles_tileset = std::move(maybe_porytiles_tileset.value());
 
     // 2. Compile with the TilesetCompilerService domain service
     auto maybe_porymap_tileset = compiler_service_->CompilePrimary(*porytiles_tileset);
     if (!maybe_porymap_tileset.has_value()) {
-        return std::unexpected{fmt::format("failed to compile tileset '{}'", tileset)};
+        return std::unexpected{maybe_porymap_tileset.error()};
     }
 
     // 3. Save the resulting PorymapTileset aggregate
     const auto porymap_tileset = std::move(maybe_porymap_tileset.value());
     if (const auto maybe_save_result = porymap_repo_->Save(*porymap_tileset); !maybe_save_result.has_value()) {
-        return std::unexpected{fmt::format("failed to save tileset '{}'", tileset)};
+        return std::unexpected{maybe_save_result.error()};
     }
     return {};
 }
