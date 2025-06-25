5ce31a757332656dc3bdcd96d6b3f6e9fd9c4436
grunt-lucas <grunt.lucas@yahoo.com>
2025-06-25 12:08:22 -0700

    Added CompileSecondary to TilesetCompilerService

--- Porytiles2/include/porytiles2/domain/services/TilesetCompilerService.hpp
+++ Porytiles2/include/porytiles2/domain/services/TilesetCompilerService.hpp
@@ -9,19 +9,29 @@
 namespace porytiles {
 
 /**
- * @brief A domain service that compiles a PorytilesTileset into a PorymapTileset.
+ * @brief A domain service that provides functionality to compile PorytilesTileset to PorymapTileset.
  */
 class TilesetCompilerService {
   public:
     virtual ~TilesetCompilerService() = default;
 
     /**
-     * @brief Compiles a primary PorytilesTileset into an equivalent PorymapTileset.
+     * @brief Compiles a primary PorytilesTileset to a PorymapTileset.
      *
-     * @param porytilesTileset The PorytilesTileset to compile.
+     * @param tileset The PorytilesTileset to compile.
      * @return A pointer to the resulting PorymapTileset or a description of the compilation error.
      */
-    virtual Result<std::unique_ptr<PorymapTileset>> CompilePrimary(const PorytilesTileset &porytilesTileset) = 0;
+    virtual Result<std::unique_ptr<PorymapTileset>> CompilePrimary(const PorytilesTileset &tileset) = 0;
+
+    /**
+     * @brief Compiles a secondary PorytilesTileset to a PorymapTileset, using a given paired primary PorymapTileset.
+     *
+     * @param tileset The PorytilesTileset to compile.
+     * @param primary_tileset The paired primary PorymapTileset.
+     * @return A pointer to the resulting PorymapTileset or a description of the compilation error.
+     */
+    virtual Result<std::unique_ptr<PorymapTileset>> CompileSecondary(const PorytilesTileset &tileset,
+                                                                     const PorymapTileset &primary_tileset) = 0;
 };
 
 } // namespace porytiles
