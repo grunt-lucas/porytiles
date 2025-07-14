20c9fa9228971c3d86c3ca04eb4e75c5f58d4637
grunt-lucas <grunt.lucas@yahoo.com>
2025-07-14 12:38:02 -0700

    Making tweaks to format/tidy scripts

--- Scripts/tidy.sh
+++ Scripts/tidy.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-set -Eeuo pipefail
+set -Eeuox pipefail
+shopt -s globstar
 
 usage() {
     cat <<EOF
-Usage: tidy.sh <file ...>
+Usage: tidy.sh
+       tidy.sh <file ...>
        tidy.sh --help
 
-Run \`clang-tidy' on the provided source files.
+Run 'clang-tidy' on the provided source files. If no source files are
+provided, runs on the default set of source files (the Porytiles2 project).
+
+Use TIDY_PATH env var to specify a path to your 'clang-tidy' executable.
 
 Options:
     -h, --help      Print this help and exit.
@@ -36,7 +41,6 @@ parse_params() {
     done
 
     args=("$@")
-    [[ ${#args[@]} -lt 1 ]] && usage_exit_error
 
     return 0
 }
@@ -48,14 +52,32 @@ then
 fi
 
 parse_params "$@"
-clang-tidy \
+if [[ ${#args[@]} -lt 1 ]]; then
+    ${TIDY_PATH:-clang-tidy} \
+    -checks='cert-*' \
+    -header-filter='.*' \
+    Porytiles2/**/*.{h,hpp,cpp} \
+    -- \
+    --std=c++23 \
+    -IPorytiles2/include \
+    -Ibuild/_deps/fmt-src/include \
+    -Ibuild/_deps/cli11_proj-src/include \
+    -Ibuild/_deps/gsl-src/include \
+    -Ibuild/_deps/cimg-src \
+    -Ibuild/_deps/googletest-src/googletest/include \
+    "$(pkg-config --cflags libpng)"
+else
+    ${TIDY_PATH:-clang-tidy} \
     -checks='cert-*' \
     -header-filter='.*' \
     "${args[@]}" \
     -- \
-    --std=c++20 \
+    --std=c++23 \
     -Iinclude \
-    $(pkg-config --cflags libpng) \
+    "$(pkg-config --cflags libpng)" \
     -Idoctest-2.4.11 \
     -Ipng++-0.2.9 \
     -Ifmt-10.0.0/include
+fi
+
+
