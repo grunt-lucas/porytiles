57e30fb6656081ad41c0a3fa8253db870fce3112
grunt-lucas <grunt.lucas@yahoo.com>
2025-07-22 15:43:50 -0700

    Work on Config system design

--- Porytiles2/include/porytiles2/infra/config/lazy_layered_config.hpp
+++ Porytiles2/include/porytiles2/infra/config/lazy_layered_config.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <vector>
+
 #include "porytiles2/domain/config/config.hpp"
+#include "porytiles2/infra/config/config_layer_provider.hpp"
 
 namespace porytiles2 {
 
@@ -16,6 +19,38 @@ namespace porytiles2 {
  * - provides a way to dump itself for debugging purposes
  */
 class LazyLayeredConfig final : public Config {
+  public:
+    /**
+     * @brief Constructs a LazyLayeredConfig with a list of ConfigLayerProviders in priority order, highest to lowest.
+     *
+     * @details
+     * The LazyLayeredConfig will attempt to resolve configuration values by traversing the provided list of
+     * ConfigLayerProviders in order. That is, the first provider in the list will be consulted first, and the next
+     * provider will only be consulted if the first does not supply the config value. And so on. It is the programmer's
+     * responsibility to provide a default layer as the final provider in the list. If any config value resolution call
+     * chain reaches the end of the provider list without finding a value, the LazyLayeredConfig will terminate with a
+     * panic.
+     *
+     * @param providers The list of providers in priority order
+     */
+    explicit LazyLayeredConfig(std::vector<std::unique_ptr<ConfigLayerProvider>> &&providers)
+        : providers_{std::move(providers)} {}
+
+    /**
+     * @brief foo
+     *
+     * @tparam Args bar
+     * @param args baz
+     * @return bat
+     */
+    template <typename... Args>
+    static LazyLayeredConfig create(Args &&...args) {
+        static_assert((std::is_base_of_v<ConfigLayerProvider, Args> && ...));
+        std::vector<std::unique_ptr<ConfigLayerProvider>> providers;
+        providers.reserve(sizeof...(args));
+        (providers.push_back(std::forward<Args>(args)), ...);
+        return LazyLayeredConfig{std::move(providers)};
+    }
 
     /*
      * Fieldmap Settings
@@ -42,6 +77,10 @@ class LazyLayeredConfig final : public Config {
      */
 
     [[nodiscard]] IncrementalBuildMode incremental_build_mode(const std::string &tileset_name) const override;
+
+  private:
+    // Providers in priority order (highest first)
+    std::vector<std::unique_ptr<ConfigLayerProvider>> providers_;
 };
 
 } // namespace porytiles2
