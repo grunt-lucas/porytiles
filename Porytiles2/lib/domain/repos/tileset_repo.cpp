59785d41b620eef282937c5e5489ea6d00c0bbc4
grunt-lucas <grunt.lucas@yahoo.com>
2025-08-07 18:18:08 -0400

    Working on TilesetRepo main implementation

--- Porytiles2/lib/domain/repos/tileset_repo.cpp
+++ Porytiles2/lib/domain/repos/tileset_repo.cpp
@@ -5,19 +5,26 @@
 
 namespace porytiles2 {
 
+/*
+ * TODO: how can we make these operations atomic, specifically the save operation?
+ */
+
+/*
+ * TODO: we need better error handling, specifically the std::unexpected returns should be more descriptive
+ */
+
 Result<void> TilesetRepo::save(const Tileset &tileset) const {
-    // auto metatiles_key = key_provider_.key_for(tileset.name(), TilesetArtifact{metatiles_bin});
-    // auto attr_key = key_provider_.key_for(tileset.name(), TilesetArtifact{metatile_attr_bin});
-    //
-    // writer_.write(metatiles_key, TilesetArtifact{metatiles_bin}, tileset);
-    // writer_.write(attr_key, TilesetArtifact{metatile_attr_bin}, tileset);
+    using enum TilesetArtifact::Type;
+
+    auto metatiles_key = key_provider_->key_for(tileset.name(), TilesetArtifact{metatiles_bin});
+    writer_->write(metatiles_key, TilesetArtifact{metatiles_bin}, tileset);
+    auto attr_key = key_provider_->key_for(tileset.name(), TilesetArtifact{metatile_attributes_bin});
+    writer_->write(attr_key, TilesetArtifact{metatile_attributes_bin}, tileset);
+
+    // TODO: fill in rest of the artifacts...
 
     // TODO: we should "clear" the stale contents of the tileset on disk after saving. That way, if the user e.g.
     // removed an anim, the stale Porymap version of the anim doesn't remain on disk and clutter the filesystem.
-
-    // if (auto save_result = save_tileset(tileset); !save_result.has_value()) {
-    //     return save_result;
-    // }
 
     const auto current_checksums = metadata_provider_->compute_artifact_checksums(tileset.name());
     return metadata_provider_->cache_checksums(tileset.name(), current_checksums);
@@ -59,29 +66,29 @@ Result<std::unique_ptr<Tileset>> TilesetRepo::load(const std::string &name) cons
     // TODO: in this case, bottom.png does not map directly onto a field of PorytilesTilesetComponent, how to handle?
     reader_->read(*tileset, bottom_png_key, TilesetArtifact{bottom_png});
 
-    // More artifacts...
+    // TODO: fill in rest of the required artifacts...
 
     // Now load optional artifacts, e.g. attributes csv, pal overrides, anims, etc.
     // attributes.csv
-    auto attr_csv_key = key_provider_->key_for(tileset->name(), TilesetArtifact{attributes_csv});
-    if (key_provider_->exists(attr_csv_key)) {
+    if (const auto attr_csv_key = key_provider_->key_for(tileset->name(), TilesetArtifact{attributes_csv});
+        key_provider_->exists(attr_csv_key)) {
         reader_->read(*tileset, attr_csv_key, TilesetArtifact{attributes_csv});
     } else {
-        // Emit warning to user about missing attr csv
+        // TODO: emit warning to user about missing attr csv
     }
 
     // palette overrides
     constexpr int num_pals = 6; // TODO: Get this from Tileset class or config
     for (int i = 0; i < num_pals; i++) {
-        auto override_key = key_provider_->key_for(tileset->name(), TilesetArtifact{override_n, i});
-        if (key_provider_->exists(override_key)) {
+        if (auto override_key = key_provider_->key_for(tileset->name(), TilesetArtifact{override_n, i});
+            key_provider_->exists(override_key)) {
             reader_->read(*tileset, override_key, TilesetArtifact{override_n, i});
         }
     }
 
     // porytiles anims
-    std::set<std::string> porytiles_anims = key_provider_->discover_porytiles_anims(tileset->name());
-    for (const auto &anim : porytiles_anims) {
+    for (const std::set<std::string> porytiles_anims = key_provider_->discover_porytiles_anims(tileset->name());
+         const auto &anim : porytiles_anims) {
         // Read key frame
         auto key_frame_key = key_provider_->key_for(tileset->name(), TilesetArtifact{porytiles_anim_key_frame, anim});
         if (!key_provider_->exists(key_frame_key)) {
@@ -115,7 +122,32 @@ Result<std::unique_ptr<Tileset>> TilesetRepo::load(const std::string &name) cons
         }
     }
 
-    // porymap anims would be the same, but without key frame logic
+    // porymap anims
+    for (const std::set<std::string> porymap_anims = key_provider_->discover_porymap_anims(tileset->name());
+         const auto &anim : porymap_anims) {
+        // Read frame 00.png
+        auto frame_00_key = key_provider_->key_for(tileset->name(), TilesetArtifact{porymap_anim_frame, anim, 0});
+        if (!key_provider_->exists(frame_00_key)) {
+            // TODO: emit validation error: missing required 00.png
+            fail_at_exit = true;
+            continue;
+        }
+        reader_->read(*tileset, frame_00_key, TilesetArtifact{porymap_anim_frame, anim, 0});
+
+        // Read the rest of the (optional) frames
+        std::set<int> frames = key_provider_->discover_porymap_anim_frames(tileset->name(), anim);
+        int expected_frame = 1;
+        for (const auto frame : frames) {
+            if (frame != expected_frame) {
+                // TODO: emit validation error: frame {} did not match expected frame {}
+                fail_at_exit = true;
+            }
+            auto frame_n_key =
+                key_provider_->key_for(tileset->name(), TilesetArtifact{porymap_anim_frame, anim, frame});
+            reader_->read(*tileset, frame_n_key, TilesetArtifact{porymap_anim_frame, anim, frame});
+            expected_frame++;
+        }
+    }
 
     if (fail_at_exit) {
         return std::unexpected{"error loading tileset"};
