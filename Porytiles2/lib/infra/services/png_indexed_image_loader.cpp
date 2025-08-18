259d98620f07d458ac386a754549f8fef3d19268
grunt-lucas <grunt.lucas@yahoo.com>
2025-08-18 11:45:26 -0700

    Added impl stubs for png index image saver and loader

---
+++ Porytiles2/lib/infra/services/png_indexed_image_loader.cpp
@@ -0,0 +1,11 @@
+#include "porytiles2/infra/services/png_indexed_image_loader.hpp"
+
+#include "porytiles2/templates/result.hpp"
+
+namespace porytiles2 {
+
+Result<std::unique_ptr<Image<std::uint8_t>>> PngIndexedImageLoader::load_from_file(const std::filesystem::path &path) const {
+    return std::unexpected{"foo"};
+}
+
+} // namespace porytiles2
---
+++ Porytiles2/lib/infra/services/png_indexed_image_saver.cpp
@@ -0,0 +1,9 @@
+#include "porytiles2/infra/services/png_indexed_image_saver.hpp"
+
+namespace porytiles2 {
+
+Result<void> PngIndexedImageSaver::save_to_file(const Image<std::uint8_t> &image, const std::filesystem::path &path) const {
+    return {};
+}
+
+} // namespace porytiles2
